c161f0b6aea2d8d4a32164130b3901b540d41441
mariaelenabotnari <152028668+mariaelenabotnari@users.noreply.github.com>
2024-10-13 14:02:17 +0300

    fixed gitignore

---
+++ src/Creature.java
@@ -0,0 +1,60 @@
+
+public class Creature {
+    private int id;
+    private Boolean isHuman;
+    private String planet;
+    private Integer age;
+    private String[] traits;
+    private String name;
+
+    public Creature() {}
+
+    public int getId () {
+        return id;
+    }
+
+    public Boolean getIsHuman () {
+        return isHuman;
+    }
+
+    public String getPlanet () {
+        return planet;
+    }
+
+    public Integer getAge () {
+        return age;
+    }
+
+    public String[] getTraits () {
+        return traits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+
+    public void setIsHuman (Boolean isHuman) {
+        this.isHuman = isHuman;
+    }
+
+    public void setPlanet (String planet) {
+        this.planet = planet;
+    }
+
+    public void setAge (Integer age) {
+        this.age = age;
+    }
+
+    public void setTraits (String[] traits) {
+        this.traits = traits;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+}
